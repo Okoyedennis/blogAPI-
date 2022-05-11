b559ca5c87f2bc8d9c6a953006b642a68a5a6206
Okoyedennis <okoyedennis7@gmail.com>
2022-05-11 13:14:36 +0100

    fashion blog api

---
+++ src/main/java/com/example/wk10_blogapi/Wk10BlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wk10_blogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk10BlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wk10BlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wk10_blogapi/Wk10BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wk10_blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk10BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
